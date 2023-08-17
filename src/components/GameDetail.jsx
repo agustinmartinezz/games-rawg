f6294b22ac20ac6a0ce13aea68f999e1b9fce9b5
Agustin Martinez <agmartinez@geocom.com.uy>
2023-08-17 17:16:47 -0300

    detail presentable

--- src/components/GameDetail.jsx
+++ src/components/GameDetail.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import { API_KEY, BASE_URL, config } from '../utils/utils'
 import Modal from 'react-modal';
 import '../styles/GameDetail.css'
+import { ClipLoader } from 'react-spinners';
 
 const customStyles = {
   content: {
@@ -22,6 +23,13 @@ const customStyles = {
 const GameDetail = ({ modalState, setModalState }) => {
   const [gameDetail, setGameDetail] = useState({})
 
+  const [platform, setPlatform] = useState("")
+  const [genre, setGenre] = useState("")
+  const [developer, setDeveloper] = useState("")
+  const [publisher, setPublisher] = useState("")
+
+  const [isLoading, setIsLoading] = useState(true)
+
   function afterOpenModal() {
     if (modalState.state) {
       document.body.style.overflow = 'hidden';
@@ -37,11 +45,10 @@ const GameDetail = ({ modalState, setModalState }) => {
       state: false,
     }
 
+    setIsLoading(true)
     setGameDetail({})
     setModalState(newState);
   }
-  
-  Modal.setAppElement('#root');
 
   const getGameDetail = async (gameId) => {
     try {
@@ -52,12 +59,41 @@ const GameDetail = ({ modalState, setModalState }) => {
         config : config
       })
 
+      setIsLoading(false)
       setGameDetail(res.data)
     } catch (error) {
       console.log("Error obteniendo detalles del juego.")
     }
   }
 
+  useEffect(() => {
+    let auxArray = []
+    if (gameDetail.platforms)
+      auxArray = gameDetail.platforms.map(platform => platform.platform.name);
+
+    setPlatform(auxArray.join(", "))
+
+    auxArray = []
+    if (gameDetail.genres)
+      auxArray = gameDetail.genres.map(genre => genre.name);
+
+    setGenre(auxArray.join(", "))
+
+    auxArray = []
+    if (gameDetail.developers)
+      auxArray = gameDetail.developers.map(developer => developer.name);
+
+    setDeveloper(auxArray.join(", "))
+
+    auxArray = []
+    if (gameDetail.publishers)
+      auxArray = gameDetail.publishers.map(publisher => publisher.name);
+
+    setPublisher(auxArray.join(", "))
+
+  },[gameDetail])
+
+  Modal.setAppElement('#root');
   return (
     <div>
       <Modal
@@ -67,8 +103,8 @@ const GameDetail = ({ modalState, setModalState }) => {
         style={customStyles}
         contentLabel="Game Details"
       >
-        <div className='modalContent'>
-          <section  style={{
+        <div className='modalBackground'>
+          <section style={{
             backgroundImage: `url(${modalState.background_image})`,
             backgroundSize: 'cover',
             backgroundRepeat: 'no-repeat',
@@ -76,10 +112,52 @@ const GameDetail = ({ modalState, setModalState }) => {
             opacity: '0.2',
             width:'100%',
             height:'100%'}} >
-              <p>{gameDetail.description_raw}</p>
-              
           </section>
         </div>
+        {
+          isLoading ?
+          <div className='modalContent d-flex justify-content-center align-items-center' style={customStyles}>
+            <ClipLoader color='#F8F9FA'/>
+          </div>
+          :
+          <div className='modalContent ' style={customStyles}>
+            <div className='text-center pt-3 h-25'>
+                <h2><a className='text-light text-decoration-none' href={gameDetail.website} target="_blank">{gameDetail.name}</a></h2>
+            </div>
+
+            <div className='d-flex flex-column justify-content-center h-75'>
+              <div className='col-12 text-center d-flex justify-content-center gap-2'>
+                <div className='col-5'>
+                  <strong className='text-light mb-0'>Platforms</strong>
+                  <div className='detailSection text-light'>
+                    {platform}
+                  </div>
+                </div>
+                <div className='col-5'>
+                  <strong className='text-light mb-0'>Genre</strong>
+                  <div className='detailSection text-light'>
+                    {genre}
+                  </div>
+                </div>
+              </div>
+              <div className='col-12 text-center mt-2 d-flex justify-content-center gap-2'>
+                <div className='col-5'>
+                  <strong className='text-light mb-0'>Developer</strong>
+                  <div className='detailSection text-light'>
+                    {developer}
+                  </div>
+                </div>
+                <div className='col-5'>
+                  <strong className='text-light mb-0'>Publisher</strong>
+                  <div className='detailSection text-light'>
+                    {publisher}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>          
+        }
+
       </Modal>
     </div>
   )
