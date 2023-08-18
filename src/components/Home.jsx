786409354eba6ff99367706349e07c1eb3a7dc75
Agustin Martinez <agmartinez@geocom.com.uy>
2023-08-18 14:52:38 -0300

    avances estéticos

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import Navigation from './Navigation'
+import '../styles/Home.css'
+import Footer from './Footer'
 
 const Home = () => {
   return (
-    <div className='container text-center'>
-      <h1>Sitio creado por Agustin Martinez</h1>
+    <div className='homeContainer d-flex flex-column justify-content-between'>
+      <Navigation />
+      {/* <div className='container text-center'>
+        <h1 className='text-light'>Sitio creado por Agustin Martinez</h1>
+      </div> */}
+      <Footer />
     </div>
+    
     
   )
 }
